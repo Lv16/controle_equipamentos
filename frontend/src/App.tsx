d4fa37e51ba0761a2fff7c9f7c9a45d4fe7c01bc
gabrielandrade131 <gabrielandrade131@gmail.com>
2026-04-08 15:39:01 -0300

    feat: add create production DTO and update app header title

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Produção de equipamentos</p>
+        <h1>Sistema de Controle de Equipamentos</h1>
       </header>
     </div>
   );
